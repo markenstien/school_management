f767e8ce1252a05a480ae6359f9191ed01217ab3
= <gonzalesmarkangeloph@gmail.com>
2023-01-25 09:47:51 +0800

    final updates

---
+++ app/libraries/simplexlsx/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0b838e2907d87097dbbee4a44c2fdf9977516737',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0b838e2907d87097dbbee4a44c2fdf9977516737',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'shuchkin/simplexlsx' => array(
+            'pretty_version' => '1.0.18',
+            'version' => '1.0.18.0',
+            'reference' => 'e8bbe7f682d4ffa1a99d4094d1c0ffcf5b82a77a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../shuchkin/simplexlsx',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
